d2e6fb729917986fffc3c9b5354375033de6f6ab
I <ibelyalov@yandex.ru>
2017-06-04 07:49:07 +0300

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pandas_keeper',
+      version='0.1',
+      description='Some helpers',
+      url='',
+      author='Ildar Belyalov',
+      author_email='ibelyalov@yandex.ru',
+      license='MIT',
+      packages=['pandas_keeper'],
+      zip_safe=False)
